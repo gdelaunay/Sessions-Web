1ea46b0154c941713c66f333db117bb7ce9b0e29
GDelaunay <goulwendelaunay@gmail.com>
2025-06-22 22:11:54 +0200

    setup front, layout, css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent implements AfterViewInit {
   title = 'SurfSessions-Web';
 
