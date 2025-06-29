4133b5bfb02eb4dba0bcadbc45e84855bf8120df
GDelaunay <goulwendelaunay@gmail.com>
2025-06-29 22:16:58 +0200

    fix footer placement

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FooterComponent} from '../footer/footer.component';
+import {HttpClient} from '@angular/common/http';
+import {ForecastService} from '../../services/forecast.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
-    FooterComponent
+    FooterComponent,
+    NgIf
   ],
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  forecast: any;
+
+  constructor(private http: HttpClient, private forecastService: ForecastService) {  }
+
+  ngOnInit() {
+    this.forecastService.getMarineForecastDaily(52.25, 21.15).
+    subscribe({
+      next: (data) => {
+        this.forecast = data;
+      },
+      error: (err) => {
+        console.error('Erreur :', err);
+      }
+    });
+  }
 
 }
