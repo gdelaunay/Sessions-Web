d894415287a1b7cd94721700289b2f54a92b85fb
GDelaunay <goulwendelaunay@gmail.com>
2025-06-30 00:02:28 +0200

    draft display marine forecast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,56 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit() {
 
-    let card = document.getElementById('testCard');
-    if (!card) return;
+    // Animation 3D de la carte/container principal
+    setTimeout(() => {
+      let cards = document.getElementsByClassName('card');
+      if (cards.length == 0) return;
 
-    card.addEventListener('mousemove', (e) => {
-      const rect = card.getBoundingClientRect();
-      const x = (e.clientX - rect.left) / rect.width - 0.5;
-      const y = (e.clientY - rect.top) / rect.height - 0.5;
+      // Animation inactivité
+      let animationFrame: number = 0;
 
-      const rotX = y * 10;
-      const rotY = x * 10;
-      card.style.transform = `rotateX(${-rotX}deg) rotateY(${rotY}deg)`;
-    });
+      function startIdleAnimation(card: HTMLElement) {
+        let angle = 0;
+        function animate() {
 
-    card.addEventListener('mouseleave', () => {
-      card.style.transform = 'rotateX(0deg) rotateY(0deg)';
-    });
+          angle += 0.01;
+          const rotX = Math.sin(angle) * 5;
+          const rotY = Math.cos(angle) * 5;
+          const translateY = Math.sin(angle) * 7.5;
+
+          card.style.transform = `translateY(${translateY}px) rotateX(${-rotX}deg) rotateY(${rotY}deg)`;
+
+          animationFrame = requestAnimationFrame(animate);
+        }
+        animate();
+      }
+      function stopIdleAnimation() {
+        cancelAnimationFrame(animationFrame);
+      }
+
+      // Animation interaction avec la souris
+      for (var i = 0; i < cards.length; i++) {
+        const card = cards[i] as HTMLElement;
+
+        startIdleAnimation(card);
+
+        card.addEventListener('mousemove', (e) => {
+          stopIdleAnimation();
+          const rect = card.getBoundingClientRect();
+          const x = (e.clientX - rect.left) / rect.width - 0.5;
+          const y = (e.clientY - rect.top) / rect.height - 0.5;
+
+          const rotX = y * 10;
+          const rotY = x * 10;
+          card.style.transform = `rotateX(${-rotX}deg) rotateY(${rotY}deg)`;
+        });
+
+        card.addEventListener('mouseleave', () => {
+          card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+          startIdleAnimation(card);
+        });
+      }
+    }, 0);
   }
 
 }
