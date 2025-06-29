eefd39fccf08f213a53bbc1b8addc8c2b8958166
GDelaunay <goulwendelaunay@gmail.com>
2025-06-29 19:11:42 +0200

    add HomeComponent et FooterComponent

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,30 @@
+import {AfterViewInit, Component, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html'
+})
+export class FooterComponent implements AfterViewInit, OnDestroy {
+  private intervalId: any;
+
+  ngAfterViewInit() {
+    const yearEl = document.getElementById('year');
+    const datetimeEl = document.getElementById('datetime');
+
+    if (yearEl && datetimeEl) {
+      const now = new Date();
+      yearEl.textContent = now.getFullYear().toString();
+      datetimeEl.textContent = now.toLocaleString();
+
+      this.intervalId = setInterval(() => {
+        datetimeEl.textContent = new Date().toLocaleString();
+      }, 1000);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+}
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {FooterComponent} from '../footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    FooterComponent
+  ],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+
+}
