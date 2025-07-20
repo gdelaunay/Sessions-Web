c69369d40d3588472c2ac57e862aeb52379a7b06
gdelaunay <goulwendelaunay@gmail.com>
2025-07-21 00:18:47 +0200

    dev écran Invité

--- src/app/components/guest/guest.component.ts
+++ src/app/components/guest/guest.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FooterComponent} from '../footer/footer.component';
-import {NgForOf, NgIf} from '@angular/common';
+import {NgIf} from '@angular/common';
 import {HttpClient} from '@angular/common/http';
 import {ForecastService} from '../../services/forecast.service';
-import * as L from 'leaflet';
 import {ForecastComponent} from '../forecast/forecast.component';
 import {MapComponent} from '../map/map.component';
+import {roundTo} from '../../utils';
 
 @Component({
   selector: 'app-guest',
@@ -17,16 +17,23 @@ import {MapComponent} from '../map/map.component';
   ],
   templateUrl: './guest.component.html'
 })
-export class GuestComponent implements OnInit {
+export class GuestComponent {
 
   forecasts: any;
   error: any;
   errorUrl: any;
+  loading: boolean = false;
 
   constructor(private http: HttpClient, private forecastService: ForecastService) {  }
 
-  ngOnInit() {
-    this.forecastService.getGuestForecast(47.124498, -2.216052).
+  @ViewChild('validateCoordsBtn') validateCoordsBtn!: ElementRef;
+  @ViewChild('latInput') latInput!: ElementRef;
+  @ViewChild('lonInput') lonInput!: ElementRef;
+
+  getForecasts(){
+    this.validateCoordsBtn.nativeElement.blur();
+    this.loading = true;
+    this.forecastService.getGuestForecast(this.latInput.nativeElement.value, this.lonInput.nativeElement.value).
     subscribe({
       next: (data) => {
         this.forecasts = data;
@@ -37,6 +44,12 @@ export class GuestComponent implements OnInit {
         this.errorUrl = err.url;
       }
     });
+    this.loading = false;
+  }
+
+  setCoords(e: { lat: number; lon: number }) {
+    this.latInput.nativeElement.value = roundTo(e.lat, 6);
+    this.lonInput.nativeElement.value = roundTo(e.lon, 6);
   }
 
 }
