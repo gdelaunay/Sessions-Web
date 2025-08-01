12c52abfb70374e0d60f7b4c195deb97c8d1eb24
gdelaunay <goulwendelaunay@gmail.com>
2025-08-01 16:19:59 +0200

    refacto (merge app.config in main) et add FR locale

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
+import {ApplicationConfig, LOCALE_ID, provideZoneChangeDetection} from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideHttpClient} from '@angular/common/http';
+import {provideRouter} from '@angular/router';
+import {routes} from './app/app.routes';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
+  ]
+};
+
+registerLocaleData(localeFr);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
