36d59dfec57c05a4e9717ef3aba420deb53eb010
gdelaunay <goulwendelaunay@gmail.com>
2025-08-10 16:58:41 +0200

    ajout TimeoutInterceptor (http)

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,20 @@ import {ApplicationConfig, LOCALE_ID, provideZoneChangeDetection} from '@angular
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
-import {provideHttpClient} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideRouter} from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import {routes} from './app/app.routes';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
+import {timeoutInterceptor} from './app/utils';
 
 
 const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([timeoutInterceptor])),
     { provide: LOCALE_ID, useValue: 'fr-FR' },
     provideAnimations(),
     provideToastr()
